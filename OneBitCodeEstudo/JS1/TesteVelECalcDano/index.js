8f54cd39b0d26be11b0d33587700a13e680ab81b
TsubasaWoKudasai <89168460+TsubasaWoKudasai@users.noreply.github.com>
2023-01-24 12:15:19 -0300

    Update index.js

--- OneBitCodeEstudo/JS1/TesteVelECalcDano/index.js
+++ OneBitCodeEstudo/JS1/TesteVelECalcDano/index.js
@@ -14,28 +14,28 @@
 
 // Calculo de dano
 
-const Ataca = prompt("qual o nome do personagem1 ?")
-const Poder1 = prompt("qual seu poder de ataque ?")
+const ataca = prompt("qual o nome do personagem1 ?")
+const poder1 = prompt("qual seu poder de ataque ?")
 
-const Defende = prompt("qual o nome do personagem2 ?")
-let Vida2 = prompt("qual a vida do personagem2 ?")
-const Def2 = prompt("qual a defesa do personagem2 ?")
-const Escudo = prompt("o personagem2 possui escudo?")
+const defende = prompt("qual o nome do personagem2 ?")
+let vida2 = prompt("qual a vida do personagem2 ?")
+const def2 = prompt("qual a defesa do personagem2 ?")
+const escudo = prompt("o personagem2 possui escudo?")
 
-let Dano = 0
+let dano = 0
 
 
-if( Poder1 > Def2 && Escudo == "nao"){
-    Dano = Poder1 - Def2
-} else if ( Poder1 > Def2 && Escudo != "nao") { 
-    Dano = ( Poder1 - Def2 ) / 2 
+if( poder1 > def2 && escudo == "nao"){
+    dano = poder1 - def2
+} else if ( poder1 > def2 && escudo != "nao") { 
+    dano = ( poder1 - def2 ) / 2 
 }
 
-Vida2 -= Dano
+vida2 -= dano
 
-alert(Ataca + " causou " + Dano + " pontos de dano em " + Defende)
+alert(ataca + " causou " + dano + " pontos de dano em " + defende)
 alert(
-  Ataca + "\nPoder de ataque: " + Poder1 + "\n\n" +
-  Defende + "\nPontos de vida: " + Vida2 +
-  "\nPoder de defesa: " + Def2 + "\nPossui escudo: " + Escudo
+  ataca + "\nPoder de ataque: " + poder1 + "\n\n" +
+  defende + "\nPontos de vida: " + vida2 +
+  "\nPoder de defesa: " + def2 + "\nPossui escudo: " + escudo
 )
