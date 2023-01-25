d096c16af3e6ae437e5726d760f705db1e9c41d9
TsubasaWoKudasai <89168460+TsubasaWoKudasai@users.noreply.github.com>
2023-01-25 09:06:52 -0300

    Update calculadora.js

--- OneBitCodeEstudo/JS1/CalcECadastro/calculadora.js
+++ OneBitCodeEstudo/JS1/CalcECadastro/calculadora.js
@@ -1,8 +1,3 @@
-const primeiroNome = prompt("Informe o primeiro nome do recruta:")
-const sobrenome = prompt("Informe o sobrenome do recruta:")
-const campoDeEstudo = prompt("Qual é o campo de estudo do recruta?")
-const anoDeNascimento = prompt("Qual é o ano de nascimento do recruta?")
-
 const entrada1 = prompt("Informe o primeiro número:")
 const entrada2 = prompt("Informe o segundo número:")
 
