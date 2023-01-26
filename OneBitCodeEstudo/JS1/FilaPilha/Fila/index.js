2fcb2f988539fb80700d404f3cb692dfa4127243
TsubasaWoKudasai <89168460+TsubasaWoKudasai@users.noreply.github.com>
2023-01-26 12:17:22 -0300

    Update index.js

--- OneBitCodeEstudo/JS1/FilaPilha/Fila/index.js
+++ OneBitCodeEstudo/JS1/FilaPilha/Fila/index.js
@@ -1,20 +1,29 @@
-let pacients = ["marcos","matheus","lucas","joao"]
+let patients = ["marcos","matheus","lucas","joao"]
 let option = ""
 
 do{
+    let names = ""
+  for (let i = 0; i < patients.length; i++) {
+    names += (i + 1) + "º - " + patients[i] + "\n"
+  }
+
     option = prompt(" Boa tarde, voce gostaria de : "
-    + "1-Adicionar novo paciente" 
-    + "2-Consultar um paciente"
+    + "1-Adicionar novo paciente" + "\n" 
+    + "2-Consultar um paciente" + "\n"
     + "3-Sair")
     switch( option ){
         case "1" :
             alert(" Voce escolheu a opcao 1")
-            let add = pacients.unshift(prompt ("digite o nome do paciente"))
+            let add = patients.unshift(prompt ("digite o nome do paciente"))
             break
         case "2":
             alert("Voce escolheu a opcao 2.")
-            let consulted = pacients.pop() 
+            let consulted = patients.shift() 
+            if (!consulted) {
+        alert("Não há pacientes na fila!")
+      } else {
             alert("Agora consultando " + consulted )
+      }
             break
         case "3":
             alert("Voce escolheu a opcao 3.")
